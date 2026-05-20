d4c65a6a8e89dd0ba1b614cf0a7794eef0b3a5c7
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2026-05-20 21:38:13 +0300

    Delete Module12/Assets/!_Project/Materials directory

--- Module12/Assets/!_Project/Materials/Scripts/Utility/BasePerk.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class BasePerk : BaseUtility
-{
-    public bool IsActive { get; protected set; } = false;
-}
--- Module12/Assets/!_Project/Materials/Scripts/Utility/BaseUtility.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class BaseUtility : MonoBehaviour
-{
-    public abstract void Execute(GameObject target);
-}
